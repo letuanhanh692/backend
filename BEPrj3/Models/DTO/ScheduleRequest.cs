1b365ee2e9f7d10e65eb266d51094f2419adfff5
letuanhanh692 <leluongtuananh6925@gmail.com>
2025-02-19 21:02:10 +0700

    backend staff 19/2

--- BEPrj3/Models/DTO/ScheduleRequest.cs
+++ BEPrj3/Models/DTO/ScheduleRequest.cs
@@ -7,7 +7,7 @@
         public int RouteId { get; set; }
         public DateTime DepartureTime { get; set; }
         public DateTime ArrivalTime { get; set; }
-        public DateTime Date { get; set; } // Có thể không dùng đến trong logic hiện tại nhưng để giữ tính linh hoạt
+        public DateTime Date { get; set; }
     }
 
 }
